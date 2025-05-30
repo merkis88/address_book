cbf211820fc784ca14564a792599fbf4fa05be7f
merkis88 <axuenno88@gmail.com>
2025-05-30 14:38:13 +0700

    first commit

---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'address_book',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+
+
+
